4be7855bcbd0b91ad5268499ebc50812bdfcb55f
Brett Adamson <adamsonb12@gmail.com>
2017-12-21 14:19:34 -0800

    created album list component

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 
 // Custom Components
 import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
+import { View } from 'react-native';
 
 export default class App extends Component<{}> {
   render() {
     return (
+      <View>
         <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
     );
   }
 }
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import Header from './src/components/header';
 
 AppRegistry.registerComponent('albums', () => App);
