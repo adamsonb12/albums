e6f10cad3f9769a8d7ddaa526fa3e1f0b80e2402
Brett Adamson <adamsonb12@gmail.com>
2017-12-21 17:31:46 -0800

    list of albums from get api listed on main activity

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
+class AlbumList extends Component {
+    state = {
+        albums: []
+    };
+  componentWillMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+      return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
+  render() {
     return (
-        <View>
-            <Text>Album List</Text>
-        </View>
+      <View>
+        {this.renderAlbums()}
+      </View>
     );
-};
+  }
+}
 
 export default AlbumList;
